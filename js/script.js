2d049e18a48ccbe66d3af47b23e0b5800b99bb25
Wadzim <w4dzim@gmail.com>
2021-03-27 17:53:36 +0300

    added tabs js/modified css/added some animation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,44 @@
 'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+
+  const tabHeader = document.querySelectorAll('.tabheader__item');
+  const tabContent = document.querySelectorAll('.tabcontent');
+  const tabHeaderParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabHeader.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabContent[i].classList.add('show', 'fade');
+    tabContent[i].classList.remove('hide');
+    tabHeader[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabHeaderParent.addEventListener('click', e => {
+    
+    const target = e.target;
+
+    if (target && target.matches('.tabheader__item')) {
+      tabHeader.forEach((item, i) => {
+        if (item == target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+
+    }
+
+  });
+});
